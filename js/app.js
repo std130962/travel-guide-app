8b08eb7f59a3686ba7dba5a21bc9287187406427
kiousis <kiousis@gmail.com>
2018-02-25 23:08:38 +0200

    Page init

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Dom7
-var $ = Dom7;
+var $$ = Dom7;
 
 var app = new Framework7({
     // App root element
@@ -16,22 +16,36 @@ var app = new Framework7({
     routes: [
         {
             path: '/about/',
-            url: 'pages/about.html',
+            url: 'pages/about.html'
         },
         {
             path: '/places/',
-            url: 'pages/places.html',
+            url: 'pages/places.html'
         },
         {
+            name: 'beaches',
             path: '/beaches/',
-            url: 'pages/beaches.html',
+            url: 'pages/beaches.html'
         },
         {
             path: '/sights/',
             url: 'pages/sights.html'
+        },
+        {
+            path: '/details/',
+            url: 'pages/details.html'
         }
     ],
     // ... other parameters
 });
 
 var mainView = app.views.create('.view-main');
+
+$$(document).on('page:init', '.page[data-name="places"]', function (e) {
+    console.log("places...");
+});
+
+$$(document).on('page:init', '.page[data-name="beaches"]', function (e) {
+    console.log("beaches...");
+});
+
